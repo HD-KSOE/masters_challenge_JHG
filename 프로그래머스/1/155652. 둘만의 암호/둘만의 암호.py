7e781cfa16bafeca6d491ad9467367bf8660a4a8
googing <104746152+wjdfhdrnr@users.noreply.github.com>
2024-11-22 13:02:40 +0900

    [level 1] Title: 둘만의 암호, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,16 +1,18 @@
 def solution(s, skip, index):
     result = []
     dic_alp = dict()
-    alp = "abcdefghijklmnopqrstuwxyz"
-    print(len(alp))
-    """
-    for i in range(len(alp)):
-        if alp[i] in skip:
-            i -= 1
-        else:
-            dic_alp[alp[i]] = i
-            
+    alp = "abcdefghijklmnopqrstuvwxyz"
+    idx = 0
+    for i in alp: 
+        if i not in skip:
+            dic_alp[idx] = i
+            idx +=1
+    print(dic_alp) 
+    alp_dic = {v:k for k,v in dic_alp.items()}
     for s_each in s:
-        result.append(dic_alp[s_each +  index])  
+        chk = alp_dic[s_each] + index
+        chk = chk % (26-len(skip))
         
-    return result
+        result.append(dic_alp[chk]) 
+    str_result = ''.join(result)
+    return str_result
