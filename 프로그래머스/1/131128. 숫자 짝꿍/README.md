e5b89fa0abd866c551d1c868d355ae9d267660b6
googing <104746152+wjdfhdrnr@users.noreply.github.com>
2024-11-22 13:03:01 +0900

    [level 1] Title: 숫자 짝꿍, Time: 1726.23 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/README.md
+++ 프로그래머스/1/131128. 숫자 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 짝꿍 - 131128 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128) 
 
 ### 성능 요약
 
-메모리: 46.4 MB, 시간: 426.27 ms
+메모리: 53.6 MB, 시간: 1726.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 15일 01:26:49
+2024년 11월 22일 13:02:59
 
 ### 문제 설명
 
--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,11 +1,23 @@
-from collections import Counter
 def solution(X, Y):
     answer = ''
-    x = Counter(X)
-    y = Counter(Y)
+
+    x=[0 for _ in range(10)]
+    y=[0 for _ in range(10)]
+
+    for i in range(max(len(X),len(Y))):
+        if len(X) > i :
+            x[int(X[i])] += 1
+        if len(Y) > i :
+            y[int(Y[i])] += 1
+            
+    for j in range(10):
+        answer += str(j)*min(x[j],y[j])
     
-    for i in x :
-        if i in y :
-            answer += i*min(x[i],y[i])
-    if answer !='' and answer.count('0') == len(answer) : return '0' 
-    return ''.join(sorted(answer,reverse=True)) if answer!='' else "-1"
+    if answer == '':
+        return '-1'
+    
+    elif answer != '' and answer.count('0') == len(answer):
+        return '0'
+
+    return ''.join(sorted(answer,reverse=True)) 
+
